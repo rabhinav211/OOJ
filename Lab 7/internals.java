8254480589c69a949670c21009e617092a465a1c
rabhinav211 <153578832+rabhinav211@users.noreply.github.com>
2024-02-06 19:56:39 +0530

    Create internals.java

---
+++ Lab 7/internals.java
@@ -0,0 +1,25 @@
+package cie;
+
+import java.util.Scanner;
+
+public class Internals extends Student {
+
+	public int marks[] = new int[5];
+
+	public void inputStudentMarks() {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter subject " + (i + 1) + " marks: ");
+			marks[i] = sc.nextInt();
+		}
+	}
+
+	public void displayStudentMarks() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Subject " + (i + 1) + " marks: " + marks[i]);
+      
+		}
+    
+	}
+  
+}
